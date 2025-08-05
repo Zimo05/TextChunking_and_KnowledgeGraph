1bb41898589c6ed4d6b2ac27cc0e6582813e6862
Zimo <143476541+Zimo05@users.noreply.github.com>
2025-08-05 17:24:11 +0800

    Update LLM_correction.py

--- PDF_to_MD/LLM_correction.py
+++ PDF_to_MD/LLM_correction.py
@@ -1,68 +1,84 @@
-import os
+import re
 from openai import OpenAI
 from Config.Settings import settings
 from PDF_to_MD.Check_File_Type import file_name, file_judge
-from PDF_to_MD.MinerU_Transform import transform, MD_File
-
+from PDF_to_MD.MinerU_Transform import mineru_zip_path
+from Config.Settings import setting
+import zipfile
 
 class correction:
-    def __init__(self, output_path):
-        self.MD_File = MD_File
-        self.output_path = output_path
-        self.client = OpenAI(api_key="sk-16528769d4794f44a01c283443c88bec", base_url="https://api.deepseek.com")
+    def __init__(self):
+        self.api = setting.Designer['DIFY']['DIFY_Correction_API']
+        self.user = setting.Designer['DIFY']['DIFY_USER']
+        self.url = setting.Designer['DIFY']['DIFY_URL']
         self.file_judge = file_judge
         self.file_name = file_name
+        self.mineru_zip_path = mineru_zip_path
+        self.extract_dir_base = setting.Designer['Storage']['PDF_to_MD']['MD_file']
+        self.subject = setting.USER['subject']
 
-    def correct_markdown_files(self):
-        if self.output_path and not os.path.exists(self.output_path):
-            os.makedirs(self.output_path)
-        
-        results = []
-        for filename in os.listdir(self.input_path):
-            if not filename.endswith('.md'):
-                continue
-                
-            content = MD_File
-            prompt = "对于我给你的markdown文本块，请把这个文本的错误语法进行修正，不要对原内容做任何的删改"
-            output_path = os.path.join(self.output_path, filename) if self.output_path else None
-            
-            try:                
-                response = self.client.chat.completions.create(
-                    model='deepseek-chat',
-                    messages=[
-                        {"role": "system", "content": "你是个聪明的老师"},
-                        {"role": "user", "content": prompt + '\n' + '以下是markdown文本：' +  '\n' + content}
-                    ],
-                    stream=False
-                )
-                modified_md = response.choices[0].message.content
+    def pre_processing(self):
+        file_type = self.file_judge['file_type']
+        file_name_base = self.file_name
+        file_name_base = file_name_base.replace('.pdf', '')
+        extract_dir = f'{self.extract_dir_base}/{file_type}/{self.subject}/{file_name_base}/'
+        with zipfile.ZipFile(self.mineru_zip_path, 'r') as zip_ref:
+            zip_ref.extractall(extract_dir)
 
-                if output_path:
-                    with open(output_path, 'w', encoding='utf-8') as file:
-                        file.write(modified_md)
-                
-                results.append(f"Success: {filename}")
-                
-            except Exception as e:
-                results.append(f"Error processing {filename}: {str(e)}")
+        mineru_folder = self.pre_processing()
+        md_content_path = f'{mineru_folder}/full.md'
+        with open(md_content_path, 'r', encoding='utf-8') as f:
+            content = f.read()
+
+        content = content.replace('．', '.')
+        content = content.replace('（', '(')
+        content = content.replace('）', ')')
         
-        return results
+        processed_text = re.sub(r'(\d+\.)(\S)', r'\1 \2', content)
+        processed_text = re.sub(r'^(\d+\.)', r'## \1', processed_text, flags=re.MULTILINE)
+
+        return processed_text, md_content_path
     
+    def correct_markdown_files(self, content, md_content_path):
+        
+        prompt = "对于我给你的markdown文本块，请把这个文本的markdown错误语法进行修正，不要对原内容做任何的删改"           
+        response = self.client.chat.completions.create(
+            model='deepseek-chat',
+            messages=[
+                {"role": "system", "content": "你是个聪明的markdown专家"},
+                {"role": "user", "content": prompt + '\n' + '以下是markdown文本：' +  '\n' + content}
+            ],
+            stream=False
+        )
+        modified_md_chunk = response.choices[0].message.content
+
+        return modified_md_chunk
     
     def main(self):
         # 
+        corrected_md_file = ''
         if self.file_judge[0] == 'Book':
+            return
 
+        else:
+            content, md_content_path = self.pre_processing()
+            sections = re.split(r'(# [一二三四五六七八九十]+、)', content)
 
+            result = []
+            for i in range(0, len(sections), 2):
+                if i + 1 < len(sections):
+                    result.append(sections[i] + sections[i+1])
+                else:
+                    result.append(sections[i])
+            for idx, sec in enumerate(result, 1):
+                modified_md = self.correct_markdown_files(sec.strip(), md_content_path)
+                corrected_md_file = corrected_md_file + '\n' + modified_md
 
+        with open(md_content_path, 'w', encoding='utf-8') as file:
+            file.write(corrected_md_file)
 
+        return corrected_md_file
 
-    
 # 做出可以识别出
 correction = correction()
 results = correction.main()
-
-# 假如是一张试卷的话，就整张卷子传，假如是一本书的话，两个##为单位传递， 然后添加上结构化的#，##，###， ####
-# 让用户自己取output的名字
-
-# 假如input一本书，results就是两个##，要做拼接
