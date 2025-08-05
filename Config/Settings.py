85e0fb1fea014e1876face50ec3816fc7f4c85fa
Zimo <143476541+Zimo05@users.noreply.github.com>
2025-08-05 10:28:16 +0800

    Update Settings.py

--- Config/Settings.py
+++ Config/Settings.py
@@ -1,29 +1,29 @@
 class settings:
     Designer = {
         'DIFY': {
-            'DIFY_ENG_Paper_Parser_API': 'app-j0NdoLnzwvbpQcMVO79XckIZ',
-            'DIFY_Entity_Linking_API': 'app-hMrsW6S7ECwARMQORH2tS5oU',
-            'DIFY_Entity_Book_Linking_API': 'app-ICVmh2I94iQ6hFxkvJ7wevLX',
-            'DIFY_Correction_API': 'app-iwGbYYWNZ1aK9TgXSLU9uIJq',
+            'DIFY_ENG_Paper_Parser_API': '',
+            'DIFY_Entity_Linking_API': '',
+            'DIFY_Entity_Book_Linking_API': '',
+            'DIFY_Correction_API': '',
             'DIFY_URL': "http://localhost/v1/workflows/run",
-            'DIFY_USER': 'Zimo'
+            'DIFY_USER': ''
             },
         'NEO4J': {
-            'NEO4J_URL': 'bolt://localhost:7474',
-            'NEO4J_USERNAME': 'neo4j',
-            'NEO4J_PASSWORD': 'neo4j20227'
+            'NEO4J_URL': '',
+            'NEO4J_USERNAME': '',
+            'NEO4J_PASSWORD': ''
             },
         'ES': {
-            'ES_URL': 'http://localhost:9200',
-            'ES_USER': 'elastic',
-            'ES_PASSWORD': 'Shenzimo123456'
+            'ES_URL': '',
+            'ES_USER': '',
+            'ES_PASSWORD': ''
             },
         'DEEPSEEK': {
             'API': 'sk-16528769d4794f44a01c283443c88bec'
             },
         'Storage':{
                 'PDF_to_MD':{
-                    'MD_file':'./Data/Original/教材/高中数学必修第一册A版/高中数学必修第一册A版.md' 
+                    'MD_file':'' 
                 },
                 'Parser':{
                     'Chunked_paper':'./Data/Updated/Papers/',
