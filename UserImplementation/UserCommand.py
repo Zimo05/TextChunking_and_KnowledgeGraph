c37cd94dc08f13d51cee873c5926a4d4906fb651
Zimo <143476541+Zimo05@users.noreply.github.com>
2025-08-08 17:23:09 +0800

    Update UserCommand.py

--- UserImplementation/UserCommand.py
+++ UserImplementation/UserCommand.py
@@ -1,10 +1,4 @@
-from PDF_to_MD.MinerU_Transform import MD_File
-from PDF_to_MD.LLM_correction import results
-from Parser.Invoke_Parser import invoking_Parser
-from Config.Settings import setting
-
 import json
-
 from openai import OpenAI
 
 # 知识图谱助手：
@@ -23,6 +17,7 @@ class Implement:
         choice = input("你的选择(如:1,3,5): ").strip()
         selected_indices = [int(idx.strip())-1 for idx in choice.split(',')]
         selected_subjects = [knowledge_options[idx] for idx in selected_indices]
+        from Config.Settings import setting
         setting.USER['knowledge'] = selected_subjects
         
         
@@ -46,28 +41,42 @@ class Implement:
         choice = input("Please choose: ").strip()
         selected_subject = knowledge_options[int(choice)-1]
         subject_code = selected_subject.split('(')[1].split(')')[0]
+        from Config.Settings import setting
         setting.USER['subject'] = subject_code
-
         # Storage path
-        setting.USER['file_path'] =  input('Please enter your file path: ') #'./Data/Original/教材/高中数学必修第一册A版/高中数学必修第一册A版.md'
-        TransformedMD = MD_File
-        chunk, df = invoking_Parser.invoke_parser()
-        return
+        setting.USER['file_path'] = input('Please enter your file path: ').strip("'\"") #'./Data/Original/教材/高中数学必修第一册A版/高中数学必修第一册A版.md'
 
+        from PDF_to_MD.Check_File_Type import CheckFileType
+        check = CheckFileType()
+        file_name = check.get_filename_from_path()
+        file_judge = check.File_type()
 
-knowledge_options = ['语文(CHI)', '数学(MAT)', '英语(ENG)', '物理(PHY)', 
-                    '化学(CHM)', '生物(BIO)', '政治(POL)', '历史(HIS)', '地理(GEO)']
+        from PDF_to_MD.MinerU_Transform import PDFProcessor
+        processor = PDFProcessor(file_judge, file_name)
+        mineru_zip_path = processor.main()
 
-for i, subject in enumerate(knowledge_options, 1):
-    print(f"{i}. {subject}")
+        from PDF_to_MD.LLM_correction import correction
+        Correction = correction(mineru_zip_path, file_name, file_judge)
+        corrected_md, md_content_path = Correction.pre_processing()
+        while True:
+            check = input(f"""Please go to the "{md_content_path}" to double check the index.
+                          The proper format should be: 
+                                                1. 1 title occupy one row
+                                                2. # Only at the front of the ‘第...章’
+                          If ok, please enter ok. If not revise it, then enter ok""")
+            with open(md_content_path, 'r') as f:
+                corrected_md = f.read()
+            if check.lower() == 'ok':
+                break
+            else:
+                print("Ensure you've check the markdown file, enter 'ok'")
+        corrected_md, md_content_path = Correction.main()
 
-print("Please choose subject(Input the number):")
-choice = input("Please choose: ").strip()
-selected_subject = knowledge_options[int(choice)-1]
-subject_code = selected_subject.split('(')[1].split(')')[0]
-setting.USER['subject'] = subject_code
-
-# Storage path
-setting.USER['file_path'] =  input('Please enter your file path: ') #'./Data/Original/教材/高中数学必修第一册A版/高中数学必修第一册A版.md'
-TransformedMD = MD_File
-chunk, df = invoking_Parser.invoke_parser()
+        from Parser.Invoke_Parser import InvokeParser
+        from Parser.MD_section_parser import MD_parser, Node
+        Parser = MD_parser(md_content_path)
+        BookTree = Parser.parse_markdown_to_linked_lists()
+        parser = InvokeParser(BookTree, md_content_path, file_name, file_judge)
+        Chunked_df = parser.invoke_parser()
+        
+        return corrected_md, Chunked_df
