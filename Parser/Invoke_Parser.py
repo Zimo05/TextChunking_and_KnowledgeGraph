e68aa7f635f141b89815abaf2b1b98a907ef43d1
Zimo <143476541+Zimo05@users.noreply.github.com>
2025-08-08 17:19:31 +0800

    Update Invoke_Parser.py

--- Parser/Invoke_Parser.py
+++ Parser/Invoke_Parser.py
@@ -1,19 +1,23 @@
-from Parser.TextBook_LumberChunker import Chunker
-from Parser.md_paper_parser import paper_parser
-from Config.Settings import setting
-from PDF_to_MD.Check_File_Type import file_judge
-from openai import OpenAI
+from Parser.TextBook_LumberChunker import LumberChunker
+from Parser.md_paper_parser import PaperParser
 
 class InvokeParser:
-    def __init__(self):
+    def __init__(self, BookTree, md_content_path, file_name, file_judge):
         self.file_judge = file_judge
+        self.file_name = file_name
+        self.BookTree = BookTree
+        self.md_content_path = md_content_path
+
     def invoke_parser(self):
         if self.file_judge['file_type'] == 'Book':
             # 教材
-            chunked_book, df = Chunker.main()
+            Chunker = LumberChunker(self.BookTree, self.file_name)
+            chunked_book = Chunker.main1()
+            df = Chunker.mian2()
             return chunked_book, df
         else:
             # 试卷
+            paper_parser = PaperParser(self.md_content_path, self.file_name)
             if self.file_judge['is ENG']:
                 # 英语
                 paper_df = paper_parser.ENG_parser()
@@ -21,6 +25,3 @@ class InvokeParser:
             else:
                 paper_df = paper_parser.GENERAL_parser()
                 return None, paper_df
-
-invoking_Parser = InvokeParser()  
-chunk, df = invoking_Parser.invoke_parser()  
