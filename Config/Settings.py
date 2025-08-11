fb73bcfbf1d71dfb4e6d2e416966d1ce69fdcf27
Zimo <143476541+Zimo05@users.noreply.github.com>
2025-08-11 16:58:15 +0800

    Update Settings.py

--- Config/Settings.py
+++ Config/Settings.py
@@ -1,14 +1,14 @@
 class settings:
     Designer = {
         'DIFY': {
-            'DIFY_ENG_Paper_Parser_API': 'app-poOfJ4k822C9y4kDOdP8eGLb',
-            'DIFY_Entity_Linking_API': 'app-hMrsW6S7ECwARMQORH2tS5oU',
-            'DIFY_Entity_Book_Linking_API': 'app-ICVmh2I94iQ6hFxkvJ7wevLX',
+            'DIFY_ENG_Paper_Parser_API': '',
+            'DIFY_Entity_Linking_API': '',
+            'DIFY_Entity_Book_Linking_API': '',
             'DIFY_Correction_API': '',
             'DIFY_URL': "http://localhost/v1/workflows/run",
-            'DIFY_USER': 'Zimo'
+            'DIFY_USER': ''
             },
-        'MinerU_Token': 'eyJ0eXBlIjoiSldUIiwiYWxnIjoiSFM1MTIifQ.eyJqdGkiOiI5MDMwOTkwMCIsInJvbCI6IlJPTEVfUkVHSVNURVIiLCJpc3MiOiJPcGVuWExhYiIsImlhdCI6MTc1NDQ2OTM0NywiY2xpZW50SWQiOiJsa3pkeDU3bnZ5MjJqa3BxOXgydyIsInBob25lIjoiIiwib3BlbklkIjpudWxsLCJ1dWlkIjoiYmJmYjA5MjAtYjE2OS00ZmFiLThjYWItZGRkM2RmNzhiYmViIiwiZW1haWwiOiIiLCJleHAiOjE3NTU2Nzg5NDd9.ltjH0meHyflsxezRgr3nKfoT2eRPEsbq04Nl4izynXGaQJ7awEcXBRbioslzsRI3Iqi-EWXewg9a6SX1YdHhog',
+        'MinerU_Token': '',
         'NEO4J': {
             'NEO4J_URL': '',
             'NEO4J_USERNAME': '',
@@ -20,16 +20,16 @@ class settings:
             'ES_PASSWORD': ''
             },
         'DEEPSEEK': {
-            'API': 'sk-16528769d4794f44a01c283443c88bec'
+            'API': ''
             },
         'Storage':{
                 'PDF_to_MD':{
-                    'MD_file':'/Users/zimoshen/Desktop/Talkweb_intern/KG_Construction/Data/MarkdownFile' # Attention please: if your saving path is ./Data/MarkdownFile/Book/MAT/必修第一册A版/, 
+                    'MD_file':'' # Attention please: if your saving path is ./Data/MarkdownFile/Book/MAT/必修第一册A版/, 
                                     # then you only need to fill ./Data/MarkdownFile in this position!!!
                 },
                 'Parser':{
-                    'Chunked_paper':'./Data/Updated/Papers',
-                    'Chunked_book': './Data/Updated/TextBooks' 
+                    'Chunked_paper':'', eg: ./Data/Updated/Papers
+                    'Chunked_book': '' eg: ./Data/Updated/TextBooks
                 },            
             },
         'Entity_linking':{
