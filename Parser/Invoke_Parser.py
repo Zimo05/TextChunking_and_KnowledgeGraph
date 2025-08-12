00a6d2996672f9df27ee0ccdbd5178c5441f034b
Zimo <143476541+Zimo05@users.noreply.github.com>
2025-08-12 15:19:34 +0800

    Update Invoke_Parser.py

--- Parser/Invoke_Parser.py
+++ Parser/Invoke_Parser.py
@@ -2,18 +2,20 @@ from Parser.TextBook_LumberChunker import LumberChunker
 from Parser.md_paper_parser import PaperParser
 
 class InvokeParser:
-    def __init__(self, BookTree, md_content_path, file_name, file_judge):
+    def __init__(self, md_content_path, file_name, file_judge):
         self.file_judge = file_judge
         self.file_name = file_name
-        self.BookTree = BookTree
         self.md_content_path = md_content_path
 
     def invoke_parser(self):
         if self.file_judge['file_type'] == 'Book':
             # 教材
-            Chunker = LumberChunker(self.BookTree, self.file_name)
+            from Parser.MD_section_parser import MD_parser
+            Parser = MD_parser(self.md_content_path)
+            BookTree = Parser.parse_markdown_to_linked_lists()
+            Chunker = LumberChunker(BookTree, self.file_name)
             chunked_book = Chunker.main1()
-            df = Chunker.mian2()
+            df = Chunker.main2()
             return chunked_book, df
         else:
             # 试卷
